702b69ef2f245822bfb6f52ce54a956d81a6afe1
Gregory Ostanin <ostingreg@gmail.com>
2019-03-20 23:37:03 -0700

    asdflkjasf

--- src/Client.java
+++ src/Client.java
@@ -5,7 +5,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Client {
-    SystemManager manager;
+    private SystemManager manager;
 
     public Client(SystemManager manager) {
         this.manager = manager;
